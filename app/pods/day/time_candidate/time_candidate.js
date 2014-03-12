df8517daf763680a342e7a36363c9b60e5c1c56b
Conrad Buck <cbuck@switchfly.com>
2014-03-11 18:41:55 -0700

    removed unused code

--- app/pods/day/time_candidate/time_candidate.js
+++ app/pods/day/time_candidate/time_candidate.js
@@ -3,17 +3,5 @@
 	App.TimeCandidateDayController = Em.IndexedArrayController.extend({
 		itemController: 'TpEntityDay',
 		colors: null,
-		// pendingCandidate: null,
-		// objectAtContent: function(idx) {
-		// 	var pendingCandidate = this.get('pendingCandidate');
-
-		// 	if(idx == this.get('content.length') && pendingCandidate) {
-		// 		return this.controllerAt(idx, pendingCandidate, this.itemController);
-		// 	}
-		// 	return this._super.apply(this, arguments);
-		// },
-		// // length: function() {
-		// // 	return this.get('content.length') + (this.get('pendingCandidate') ? 1 : 0);
-		// // }.property('content.length', 'pendingCandidate')
 	});
 })(Ember, App);
